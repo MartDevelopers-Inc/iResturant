da1693acfc24d947335e03023fa140d4ec4bc8c1
MartMbithi <martdevelopers254@gmail.com>
2021-07-10 12:42:00 +0300

    Load Logged In User Session On Header Partial

--- partials/header.php
+++ partials/header.php
@@ -1,149 +1,116 @@
-<div class="topbar">
-    <!-- Navbar -->
-    <nav class="navbar-custom">
-        <ul class="list-unstyled topbar-nav float-end mb-0">
-            <li class="dropdown hide-phone">
-                <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    <i data-feather="search" class="topbar-icon"></i>
-                </a>
-
-                <div class="dropdown-menu dropdown-menu-end dropdown-lg p-0">
-                    <!-- Top Search Bar -->
-                    <div class="app-search-topbar">
-                        <form action="#" method="get">
-                            <input type="search" name="search" class="from-control top-search mb-0" placeholder="Type text...">
-                            <button type="submit"><i class="ti-search"></i></button>
-                        </form>
-                    </div>
-                </div>
-            </li>
-
-            <li class="dropdown notification-list">
-                <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    <i data-feather="bell" class="align-self-center topbar-icon"></i>
-                    <span class="badge bg-danger rounded-pill noti-icon-badge">2</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end dropdown-lg pt-0">
-
-                    <h6 class="dropdown-item-text font-15 m-0 py-3 border-bottom d-flex justify-content-between align-items-center">
-                        Notifications <span class="badge bg-primary rounded-pill">2</span>
-                    </h6>
-                    <div class="notification-menu" data-simplebar>
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">2 min ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <i data-feather="shopping-cart" class="align-self-center icon-xs"></i>
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
-                                    <small class="text-muted mb-0">Dummy text of the printing and industry.</small>
-                                </div>
-                                <!--end media-body-->
-                            </div>
-                            <!--end media-->
-                        </a>
-                        <!--end-item-->
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">10 min ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <img src="assets/images/users/user-4.jpg" alt="" class="thumb-sm rounded-circle">
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">Meeting with designers</h6>
-                                    <small class="text-muted mb-0">It is a long established fact that a reader.</small>
-                                </div>
-                                <!--end media-body-->
-                            </div>
-                            <!--end media-->
-                        </a>
-                        <!--end-item-->
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">40 min ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <i data-feather="users" class="align-self-center icon-xs"></i>
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">UX 3 Task complete.</h6>
-                                    <small class="text-muted mb-0">Dummy text of the printing.</small>
-                                </div>
-                                <!--end media-body-->
-                            </div>
-                            <!--end media-->
-                        </a>
-                        <!--end-item-->
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">1 hr ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <img src="assets/images/users/user-5.jpg" alt="" class="thumb-sm rounded-circle">
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
-                                    <small class="text-muted mb-0">It is a long established fact that a reader.</small>
-                                </div>
-                                <!--end media-body-->
-                            </div>
-                            <!--end media-->
-                        </a>
-                        <!--end-item-->
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">2 hrs ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <i data-feather="check-circle" class="align-self-center icon-xs"></i>
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">Payment Successfull</h6>
-                                    <small class="text-muted mb-0">Dummy text of the printing.</small>
-                                </div>
-                                <!--end media-body-->
-                            </div>
-                            <!--end media-->
-                        </a>
-                        <!--end-item-->
-                    </div>
-                    <!-- All-->
-                    <a href="javascript:void(0);" class="dropdown-item text-center text-primary">
-                        View all <i class="fi-arrow-right"></i>
+<?php
+/*
+ * Created on Sat Jul 10 2021
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+$id = $_SESSION['id'];
+$ret = "SELECT * FROM `iResturant_Admin_Login` WHERE id = '$id'  ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($logged_in_user = $res->fetch_object()) {
+?>
+    <div class="topbar">
+        <!-- Navbar -->
+        <nav class="navbar-custom">
+            <ul class="list-unstyled topbar-nav float-end mb-0">
+                <li class="dropdown hide-phone">
+                    <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                        <i data-feather="search" class="topbar-icon"></i>
                     </a>
-                </div>
-            </li>
 
-            <li class="dropdown">
-                <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    <span class="ms-1 nav-user-name hidden-sm">Nick</span>
-                    <img src="assets/images/users/user-5.jpg" alt="profile-user" class="rounded-circle thumb-xs" />
-                </a>
-                <div class="dropdown-menu dropdown-menu-end">
-                    <a class="dropdown-item" href="#"><i data-feather="user" class="align-self-center icon-xs icon-dual me-1"></i> Profile</a>
-                    <a class="dropdown-item" href="#"><i data-feather="settings" class="align-self-center icon-xs icon-dual me-1"></i> Settings</a>
-                    <div class="dropdown-divider mb-0"></div>
-                    <a class="dropdown-item" href="#"><i data-feather="power" class="align-self-center icon-xs icon-dual me-1"></i> Logout</a>
-                </div>
-            </li>
-        </ul>
-        <!--end topbar-nav-->
+                    <div class="dropdown-menu dropdown-menu-end dropdown-lg p-0">
+                        <!-- Top Search Bar -->
+                        <div class="app-search-topbar">
+                            <form action="#" method="get">
+                                <input type="search" name="search" class="from-control top-search mb-0" placeholder="Type text...">
+                                <button type="submit"><i class="ti-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </li>
 
-        <ul class="list-unstyled topbar-nav mb-0">
-            <li>
-                <button class="nav-link button-menu-mobile">
-                    <i data-feather="menu" class="align-self-center topbar-icon"></i>
-                </button>
-            </li>
-            <li class="creat-btn">
-                <div class="nav-link">
-                    <a class=" btn btn-sm btn-soft-primary" href="#" role="button"><i class="fas fa-plus me-2"></i>New Task</a>
-                </div>
-            </li>
-        </ul>
-    </nav>
-    <!-- end navbar-->
-</div>
+                <li class="dropdown notification-list">
+                    <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                        <i data-feather="bell" class="align-self-center topbar-icon"></i>
+                        <span class="badge bg-danger rounded-pill noti-icon-badge">2</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end dropdown-lg pt-0">
+
+                        <h6 class="dropdown-item-text font-15 m-0 py-3 border-bottom d-flex justify-content-between align-items-center">
+                            Notifications <span class="badge bg-primary rounded-pill">2</span>
+                        </h6>
+                        <div class="notification-menu" data-simplebar>
+                            <!-- item-->
+                            <a href="#" class="dropdown-item py-3">
+                                <small class="float-end text-muted ps-2">2 min ago</small>
+                                <div class="media">
+                                    <div class="avatar-md bg-soft-primary">
+                                        <i data-feather="shopping-cart" class="align-self-center icon-xs"></i>
+                                    </div>
+                                    <div class="media-body align-self-center ms-2 text-truncate">
+                                        <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
+                                        <small class="text-muted mb-0">Dummy text of the printing and industry.</small>
+                                    </div>
+                                    <!--end media-body-->
+                                </div>
+                                <!--end media-->
+                            </a>
+                            <!--end-item-->
+
+                            <!--end-item-->
+                        </div>
+                        <!-- All-->
+                        <a href="javascript:void(0);" class="dropdown-item text-center text-primary">
+                            View all <i class="fi-arrow-right"></i>
+                        </a>
+                    </div>
+                </li>
+
+                <li class="dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                        <span class="ms-1 nav-user-name hidden-sm"><?php echo substr($logged_in_user->email, 0, 5); ?>...</span>
+                        <img src="../public/uploads/user_images/no-profile.png" alt="profile-user" class="rounded-circle thumb-xs" />
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <a class="dropdown-item" href="settings"><i data-feather="settings" class="align-self-center icon-xs icon-dual me-1"></i> Settings</a>
+                        <div class="dropdown-divider mb-0"></div>
+                        <a class="dropdown-item" href="logout"><i data-feather="power" class="align-self-center icon-xs icon-dual me-1"></i> Logout</a>
+                    </div>
+                </li>
+            </ul>
+            <!--end topbar-nav-->
+
+            <ul class="list-unstyled topbar-nav mb-0">
+                <li>
+                    <button class="nav-link button-menu-mobile">
+                        <i data-feather="menu" class="align-self-center topbar-icon"></i>
+                    </button>
+                </li>
+                <li class="creat-btn">
+                    <div class="nav-link">
+                        <a class=" btn btn-sm btn-soft-primary" href="#" role="button"><i class="fas fa-plus me-2"></i>New Task</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <!-- end navbar-->
+    </div>
+<?php
+} ?>
