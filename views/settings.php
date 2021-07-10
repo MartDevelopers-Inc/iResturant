3b32bbe0cc1fa626f2648c133a0c038cfdb9da00
MartMbithi <martdevelopers254@gmail.com>
2021-07-10 13:17:31 +0300

    Backend Logic  - Fetching Logged In User Profile Details

--- views/settings.php
+++ views/settings.php
@@ -49,52 +49,77 @@ require_once('../partials/head.php');
                 <div class="container-fluid">
                     <!-- Page-Title -->
                     <div class="row">
-                        <div class="col-sm-12">
-                            <div class="page-title-box">
-                                <div class="row">
-                                    <div class="col">
-                                        <h4 class="page-title">Administrator Profile</h4>
-                                        <ol class="breadcrumb">
-                                            <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
-                                            <li class="breadcrumb-item">Profile</li>
-                                        </ol>
-                                    </div>
-                                    <!--end col-->
-                                    <div class="col-auto align-self-center">
-                                        <a href="#" class="btn btn-sm btn-outline-primary" id="Dash_Date">
-                                            <span class="day-name" id="Day_Name">Today:</span>&nbsp;
-                                            <span class="" id="Select_date">Jan 11</span>
-                                            <i data-feather="calendar" class="align-self-center icon-xs ms-1"></i>
-                                        </a>
-                                        <a href="#" class="btn btn-sm btn-outline-primary">
-                                            <i data-feather="download" class="align-self-center icon-xs"></i>
-                                        </a>
-                                    </div>
-                                    <!--end col-->
-                                </div>
-                                <!--end row-->
-                            </div>
-                            <!--end page-title-box-->
+                        <div class="col">
+                            <h4 class="page-title">Administrator Profile Settings</h4>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Setings</li>
+                            </ol>
                         </div>
                         <!--end col-->
+                        <div class="col-sm-12">
+                            <div class="page-title-box">
+                                <div class="col-md-12">
+                                    <div class="card card-primary card-outline">
+                                        <div class="card-body">
+                                            <form method='post' class="form-horizontal">
+                                                <div class="form-group row">
+                                                    <label for="inputName" class="col-sm-2 col-form-label">Email</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="email" name="email" value="<?php echo $logged_in_user->email; ?>" required class="form-control" id="inputName">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="inputName" class="col-sm-2 col-form-label">Old Password</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="password" name="old_password" required class="form-control" id="inputName">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="inputEmail" class="col-sm-2 col-form-label">New Password</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="password" name="new_password" required class="form-control" id="inputEmail">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="inputName2" class="col-sm-2 col-form-label">Confirm New Password</label>
+                                                    <div class="col-sm-10">
+                                                        <input type="password" name="confirm_password" required class="form-control" id="inputName2">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group text-right row">
+                                                    <div class="offset-sm-2 col-sm-10">
+                                                        <button type="submit" name="change_password" class="btn btn-primary">Update Profile</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end row-->
+                        </div>
+                        <!--end page-title-box-->
                     </div>
-                    <!--end row-->
-                    <!-- end page title end breadcrumb -->
+                    <!--end col-->
+                </div>
+                <!--end row-->
+                <!-- end page title end breadcrumb -->
 
 
-                </div><!-- container -->
+            </div><!-- container -->
 
-                <?php require_once('../partials/footer.php'); ?>
-                <!--end footer-->
-            </div>
-        <?php
-        } ?>
-        <!-- end page content -->
+            <?php require_once('../partials/footer.php'); ?>
+            <!--end footer-->
     </div>
-    <!-- end page-wrapper -->
+<?php
+        } ?>
+<!-- end page content -->
+</div>
+<!-- end page-wrapper -->
 
-    <!-- App Js -->
-    <?php require_once('../partials/scripts.php'); ?>
+<!-- App Js -->
+<?php require_once('../partials/scripts.php'); ?>
 </body>
 
 
