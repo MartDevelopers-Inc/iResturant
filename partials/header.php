6dbdb90ca51fc0c50b37259f2a49c2bb00017376
MartMbithi <martdevelopers254@gmail.com>
2021-07-12 17:35:52 +0300

    Implement Notification Logic

--- partials/header.php
+++ partials/header.php
@@ -49,35 +49,44 @@ while ($logged_in_user = $res->fetch_object()) {
                 <li class="dropdown notification-list">
                     <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                         <i data-feather="bell" class="align-self-center topbar-icon"></i>
-                        <span class="badge bg-danger rounded-pill noti-icon-badge">2</span>
+                        <span class="badge bg-danger rounded-pill noti-icon-badge"><?php echo $notification_count; ?></span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-lg pt-0">
 
                         <h6 class="dropdown-item-text font-15 m-0 py-3 border-bottom d-flex justify-content-between align-items-center">
-                            Notifications <span class="badge bg-primary rounded-pill">2</span>
+                            Notifications <span class="badge bg-primary rounded-pill"><?php echo $notification_count; ?></span>
                         </h6>
                         <div class="notification-menu" data-simplebar>
-                            <!-- item-->
-                            <a href="#" class="dropdown-item py-3">
-                                <small class="float-end text-muted ps-2">2 min ago</small>
-                                <div class="media">
-                                    <div class="avatar-md bg-soft-primary">
-                                        <i data-feather="shopping-cart" class="align-self-center icon-xs"></i>
+                            <?php
+                            $ret = "SELECT * FROM `iResturant_Notification` WHERE user_id = '$id'";
+                            $stmt = $mysqli->prepare($ret);
+                            $stmt->execute(); //ok
+                            $res = $stmt->get_result();
+                            while ($notification = $res->fetch_object()) {
+                            ?>
+                                <!-- item-->
+                                <a href="notifications" class="dropdown-item py-3">
+                                    <small class="float-end text-muted ps-2"><?php echo date('d-M-Y g:ia', strtotime($notification->crated_at)); ?></small>
+                                    <div class="media">
+                                        <div class="avatar-md bg-soft-primary">
+                                            <i data-feather="bell" class="align-self-center icon-xs"></i>
+                                        </div>
+                                        <div class="media-body align-self-center ms-2 text-truncate">
+                                            <h6 class="my-0 fw-normal text-dark"><?php echo $notification->title; ?></h6>
+                                            <small class="text-muted mb-0"><?php echo substr($notification->details, 0, 15); ?>...</small>
+                                        </div>
+                                        <!--end media-body-->
                                     </div>
-                                    <div class="media-body align-self-center ms-2 text-truncate">
-                                        <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
-                                        <small class="text-muted mb-0">Dummy text of the printing and industry.</small>
-                                    </div>
-                                    <!--end media-body-->
-                                </div>
-                                <!--end media-->
-                            </a>
+                                    <!--end media-->
+                                </a>
+                            <?php
+                            } ?>
                             <!--end-item-->
 
                             <!--end-item-->
                         </div>
                         <!-- All-->
-                        <a href="javascript:void(0);" class="dropdown-item text-center text-primary">
+                        <a href="notifications" class="dropdown-item text-center text-primary">
                             View all <i class="fi-arrow-right"></i>
                         </a>
                     </div>
