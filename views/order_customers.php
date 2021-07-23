98e661913207b74a9aa9831b2f489b6ab58623f0
MartMbithi <martdevelopers254@gmail.com>
2021-07-23 10:22:24 +0300

    Implement Update Functionality On Orders

--- views/order_customers.php
+++ views/order_customers.php
@@ -61,14 +61,14 @@ if (isset($_POST['add_order'])) {
 
 /* Udpate Order */
 if (isset($_POST['update_order'])) {
-    $id = $_POST['id'];
+    $code = $_POST['code'];
     $meal_count = $_POST['meal_count'];
     $speacial_request = $_POST['speacial_request'];
     $order_amount = $_POST['order_amount'];
 
-    $query = "UPDATE iResturant_Customer_Orders SET meal_count =?, speacial_request =?, order_amount =? WHERE id = ?";
+    $query = "UPDATE iResturant_Customer_Orders SET meal_count =?, speacial_request =?, order_amount =? WHERE code = ?";
     $stmt = $mysqli->prepare($query);
-    $rc = $stmt->bind_param('ssss', $meal_count, $speacial_request, $order_amount, $id);
+    $rc = $stmt->bind_param('ssss', $meal_count, $speacial_request, $order_amount, $code);
     $stmt->execute();
     if ($stmt) {
         $success = "Order Updated";
@@ -81,7 +81,7 @@ if (isset($_POST['update_order'])) {
 /* Delete Order */
 if (isset($_GET['delete_order'])) {
     $id = $_GET['delete_order'];
-    $adn = 'DELETE FROM iResturant_Customer_Orders WHERE id=?';
+    $adn = 'DELETE FROM iResturant_Customer_Orders WHERE code=?';
     $stmt = $mysqli->prepare($adn);
     $stmt->bind_param('s', $id);
     $stmt->execute();
@@ -142,7 +142,138 @@ require_once('../partials/head.php');
                             <!--end card-header-->
                             <div class="card-body table-responsive">
                                 <div class="">
+                                    <table class="table dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                        <thead>
+                                            <tr>
+                                                <th class="border-top-0">Order Code</th>
+                                                <th class="border-top-0">Customer Details</th>
+                                                <th class="border-top-0">Ordered Meal</th>
+                                                <th class="border-top-0">Manage Orders</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $ret = "SELECT * FROM `iResturant_Currencies` WHERE status = 'Active'  ";
+                                            $stmt = $mysqli->prepare($ret);
+                                            $stmt->execute(); //ok
+                                            $res = $stmt->get_result();
+                                            while ($currency = $res->fetch_object()) {
+                                                $ret =
+                                                    "SELECT * FROM iResturant_Customer c INNER JOIN iResturant_Customer_Orders cs ON cs.customer_id = c.id 
+                                                    INNER JOIN iResturant_Menu rm
+                                                    ON rm.meal_id = cs.meal_menu_id
+                                                    /* INNER JOIN iResturant_Payments pa ON cs.code = pa.order_code  */                                                                                                                 
+                                                            ";
+                                                $stmt = $mysqli->prepare($ret);
+                                                $stmt->execute(); //ok
+                                                $res = $stmt->get_result();
+                                                while ($orders = $res->fetch_object()) {
+                                                    /* Order Bill Amount */
+                                                    $order_bill = $orders->meal_count * $orders->order_amount;
+                                            ?>
+                                                    <tr>
+                                                        <td>
+                                                            <a class="text-primary" href="order_customer?view=<?php echo $orders->code; ?>">
+                                                                <?php echo $orders->code; ?>
+                                                            </a>
+                                                        </td>
+                                                        <td>
+                                                            Name:<?php echo $orders->name; ?><br>
+                                                            Phone:<?php echo $orders->phone; ?><br>
+                                                            Email:<?php echo $orders->email; ?>
+                                                        </td>
+                                                        <td>
+                                                            Meal : <?php echo $orders->meal_name; ?><br>
+                                                            Count : <?php echo $orders->meal_count; ?><br>
+                                                            Order Bill : <?php echo $currency->code . " " . $order_bill; ?><br>
+                                                            Date Ordered: <?php echo date('d-M-Y', strtotime($orders->created_at)); ?>
+                                                        </td>
+                                                        <td>
+                                                            <?php
+                                                            if ($orders->status == 'Unpaid') {
+                                                                echo
+                                                                '
+                                                                <a href="#edit-' . $orders->code . '" data-bs-toggle="modal" data-bs-target="#edit-edit-' . $orders->code . '" class="btn btn-sm btn-outline-success">
+                                                                    <i data-feather="dollar-sign" class="align-self-center icon-xs ms-1"></i> Pay Order
+                                                                </a>
+                                                                ';
+                                                            }
+                                                            ?>
+                                                            <a href="#edit-<?php echo $orders->code; ?>" data-bs-toggle="modal" data-bs-target="#edit-<?php echo $orders->code; ?>" class="btn btn-sm btn-outline-warning">
+                                                                <i data-feather="edit" class="align-self-center icon-xs ms-1"></i> Edit
+                                                            </a>
+                                                            <a href="#delete-<?php echo $orders->code; ?>" data-bs-toggle="modal" data-bs-target="#delete-<?php echo $orders->code; ?>" class="btn btn-sm btn-outline-danger">
+                                                                <i data-feather="trash" class="align-self-center icon-xs ms-1"></i> Delete
+                                                            </a>
+                                                            <!-- Add Payment -->
+                                                            <!-- End Payment -->
 
+                                                            <!-- Edit  Modal -->
+                                                            <div class="modal fade" id="edit-<?php echo $orders->code; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalPrimary1" aria-hidden="true">
+                                                                <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                                                                    <div class="modal-content">
+                                                                        <div class="modal-header bg-warning">
+                                                                            <h6 class="modal-title m-0 text-white" id="exampleModalPrimary1">Update Customer Order</h6>
+                                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                        </div>
+                                                                        <div class="modal-body">
+                                                                            <div class="row">
+                                                                                <form method="post" enctype="multipart/form-data" role="form">
+                                                                                    <div class="card-body">
+                                                                                        <div class="row">
+                                                                                            <div class="form-group col-md-6">
+                                                                                                <label for="">Meal Count </label>
+                                                                                                <input type="number" required name="meal_count" value="<?php echo $orders->meal_count; ?>" class="form-control">
+                                                                                                <input type="hidden" required name="code" value="<?php echo $orders->code; ?>" class="form-control">
+                                                                                            </div>
+                                                                                            <div class="form-group col-md-6">
+                                                                                                <label for="">Unit Meal Price </label>
+                                                                                                <input type="text" readonly value="<?php echo $orders->order_amount; ?>" required name="order_amount" class="form-control">
+                                                                                            </div>
+                                                                                            <div class="form-group col-md-12">
+                                                                                                <label for="">Special Requests</label>
+                                                                                                <textarea name="speacial_request" class="form-control" rows="5"><?php echo $orders->speacial_request; ?></textarea>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                    <div class="text-center">
+                                                                                        <button type="submit" name="update_order" class="btn btn-primary">Submit</button>
+                                                                                    </div>
+                                                                                </form>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <!-- End Edit Modal -->
+
+                                                            <!-- Delete Room Category Modal -->
+                                                            <div class="modal fade" id="delete-<?php echo $orders->code; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                                                    <div class="modal-content">
+                                                                        <div class="modal-header">
+                                                                            <h5 class="modal-title" id="exampleModalLabel">CONFIRM</h5>
+                                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+                                                                        </div>
+                                                                        <div class="modal-body text-center text-danger">
+                                                                            <h4>Delete Order ?</h4>
+                                                                            <br>
+                                                                            <p>Heads Up, You are about to delete customer order.<br> This action is irrevisble.</p>
+                                                                            <button type="button" class="btn btn-soft-success" data-bs-dismiss="modal">No</button>
+                                                                            <a href="order_customers?delete=<?php echo $orders->code; ?>" class="text-center btn btn-danger"> Delete </a>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <!-- End Delete Modal -->
+                                                        </td>
+                                                    </tr>
+                                            <?php
+                                                }
+                                            } ?>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
