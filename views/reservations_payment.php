acd6dda5e73dc17d35f27f703c7d97117cf8e5bd
MartMbithi <martdevelopers254@gmail.com>
2021-07-28 13:07:21 +0300

    Minor Fix On Reservation Payment

--- views/reservations_payment.php
+++ views/reservations_payment.php
@@ -108,13 +108,13 @@ require_once('../partials/head.php');
                         <!-- end page title end breadcrumb -->
                         <div class="row">
                             <div class="col-lg-12">
-                                <div class="text-center">
+                                <div class="d-flex justify-content-end">
                                     <button id="print" onclick="printContent('PrintReceipt');" type="button" class="btn btn-primary">
                                         <i data-feather="printer" class="align-self-center icon-xs ms-1"></i>
                                         Print Receipt
                                     </button>
                                 </div>
-                                <br>
+                                <hr>
                                 <table class="body-wrap" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background-color: transparent; margin: 0;" bgcolor="transparent">
                                     <tr>
                                         <td valign="top"></td>
@@ -141,67 +141,70 @@ require_once('../partials/head.php');
                                                                     </tr>
                                                                     <tr>
                                                                         <td class="content-block" style="padding: 0 0 20px;" valign="top">
-                                                                            <h2 class="aligncenter" style="font-family: 'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-size: 24px; color:#50649c; line-height: 1.2em; font-weight: 600; text-align: center;" align="center">Thanks For Reserving Room At <span style="color: #4d79f6; font-weight: 700;"><?php echo $sys->name; ?></span>.</h2>
+                                                                            <h2 class="aligncenter" style="font-family: 'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-size: 24px; color:#50649c; line-height: 1.2em; font-weight: 600; text-align: center;" align="center">Thanks For Reserving Room At <span style="color: #4d79f6; font-weight: 700;"><?php echo $sys->system_name; ?></span>.</h2>
                                                                         </td>
                                                                     </tr>
-                                                                <?php
-                                                                }
-                                                                $ret = "SELECT * FROM iResturant_Customer c
+                                                                    <?php
+
+                                                                    $ret = "SELECT * FROM iResturant_Customer c
                                                                 INNER JOIN iResturant_Room_Reservation r ON c.id = r.client_id
                                                                 INNER JOIN iResturant_Room rm
                                                                 ON r.room_id = rm.id WHERE r.code = '$payment->reservation_code'                                                        
                                                                 ";
-                                                                $stmt = $mysqli->prepare($ret);
-                                                                $stmt->execute(); //ok
-                                                                $res = $stmt->get_result();
-                                                                while ($reservation = $res->fetch_object()) {
-                                                                    $checkin = strtotime($reservation->arrival);
-                                                                    $checkout = strtotime($reservation->departure);
-                                                                    $secs = $checkout - $checkin;
-                                                                    $days_reserved = $secs / 86400;
-                                                                    $total_payable_amt = $days_reserved * ($reservation->price);
-                                                                    $now = strtotime(date("Y-m-d"));
-                                                                ?>
+                                                                    $stmt = $mysqli->prepare($ret);
+                                                                    $stmt->execute(); //ok
+                                                                    $res = $stmt->get_result();
+                                                                    while ($reservation = $res->fetch_object()) {
+                                                                        $checkin = strtotime($reservation->arrival);
+                                                                        $checkout = strtotime($reservation->departure);
+                                                                        $secs = $checkout - $checkin;
+                                                                        $days_reserved = $secs / 86400;
+                                                                        $total_payable_amt = $days_reserved * ($reservation->price);
+                                                                        $now = strtotime(date("Y-m-d")); ?>
 
-                                                                    <tr>
-                                                                        <td class="content-block aligncenter" style="padding: 0 0 20px;" align="center" valign="top">
-                                                                            <table class="invoice" style="width: 80%;">
-                                                                                <tr>
-                                                                                    <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; padding: 5px 0;" valign="top">
-                                                                                        <?php echo $reservation->name; ?> <br>
-                                                                                        <?php echo $reservation->phone; ?><br>
-                                                                                        <?php echo $reservation->email; ?><br>
-                                                                                        <br />Room #<?php echo $reservation->number; ?>
-                                                                                        <br />Check In: <?php echo date('d-M-Y', strtotime($reservation->arrival)); ?>
-                                                                                        <br />Check Out: <?php echo date('d-M-Y', strtotime($reservation->departure)); ?>
-                                                                                    </td>
-                                                                                </tr>
-                                                                                <tr>
-                                                                                    <td style="padding: 5px 0;" valign="top">
-                                                                                        <table class="invoice-items" cellpadding="0" cellspacing="0" style="width: 100%;">
-                                                                                            <tr>
-                                                                                                <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 10px 0;" valign="top">Room Reservation For <?php echo $days_reserved; ?> Day(s)</td>
-                                                                                                <td class="alignright" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 10px 0;" align="right" valign="top"><?php echo $currency->code . " " . $total_payable_amt; ?></td>
-                                                                                            </tr>
+                                                                        <tr>
+                                                                            <td class="content-block aligncenter" style="padding: 0 0 20px;" align="center" valign="top">
+                                                                                <table class="invoice" style="width: 80%;">
+                                                                                    <tr>
+                                                                                        <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; padding: 5px 0;" valign="top">
+                                                                                            <?php echo $reservation->name; ?> <br>
+                                                                                            <?php echo $reservation->phone; ?><br>
+                                                                                            <?php echo $reservation->email; ?><br>
+                                                                                            <br />Room #<?php echo $reservation->number; ?>
+                                                                                            <br />Check In: <?php echo date('d-M-Y', strtotime($reservation->arrival)); ?>
+                                                                                            <br />Check Out: <?php echo date('d-M-Y', strtotime($reservation->departure)); ?>
+                                                                                        </td>
+                                                                                    </tr>
+                                                                                    <tr>
+                                                                                        <td style="padding: 5px 0;" valign="top">
+                                                                                            <table class="invoice-items" cellpadding="0" cellspacing="0" style="width: 100%;">
+                                                                                                <tr>
+                                                                                                    <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 10px 0;" valign="top">Room Reservation For <?php echo $days_reserved; ?> Day(s)</td>
+                                                                                                    <td class="alignright" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 10px 0;" align="right" valign="top"><?php echo $currency->code . " " . $total_payable_amt; ?></td>
+                                                                                                </tr>
 
-                                                                                            <tr>
-                                                                                                <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 10px 0;" valign="top">Total </td>
-                                                                                                <td class="alignright" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 2px; border-top-color: #50649c; border-top-style: solid; border-bottom-color: #50649c; border-bottom-width: 2px; border-bottom-style: solid; font-weight: 700; margin: 0; padding: 10px 0;" align="right" valign="top"> <?php echo "" . $currency->code . " " . $total_payable_amt; ?></td>
-                                                                                            </tr>
-                                                                                        </table>
-                                                                                    </td>
-                                                                                </tr>
-                                                                            </table>
-                                                                        </td>
-                                                                    </tr>
+                                                                                                <tr>
+                                                                                                    <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 10px 0;" valign="top">Total </td>
+                                                                                                    <td class="alignright" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 2px; border-top-color: #50649c; border-top-style: solid; border-bottom-color: #50649c; border-bottom-width: 2px; border-bottom-style: solid; font-weight: 700; margin: 0; padding: 10px 0;" align="right" valign="top"> <?php echo "" . $currency->code . " " . $total_payable_amt; ?></td>
+                                                                                                </tr>
+                                                                                            </table>
+                                                                                        </td>
+                                                                                    </tr>
+                                                                                </table>
+                                                                            </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td class="content-block aligncenter" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; text-align: center; margin: 0; padding: 0 0 20px;" align="center" valign="top">
+                                                                                <hr>
+                                                                                <?php echo $sys->system_name; ?>
+                                                                                <br>
+                                                                                <i><?php echo $sys->tagline; ?></i>
+                                                                            </td>
+                                                                        </tr>
                                                                 <?php
+                                                                    }
                                                                 }
                                                                 ?>
-                                                                <tr>
-                                                                    <td class="content-block aligncenter" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 14px; vertical-align: top; text-align: center; margin: 0; padding: 0 0 20px;" align="center" valign="top">
-                                                                        A <a href="https://martdev.info" target="_blank">MartDevelopers Inc</a> Production
-                                                                    </td>
-                                                                </tr>
                                                             </table>
                                                             <!--end table-->
                                                         </td>
