6fa3e49bb7d90c71d805f22c6b9c2264e4764657
MartMbithi <martdevelopers254@gmail.com>
2021-08-03 14:04:28 +0300

    Refactored Notifications From Admin Dashboard

--- partials/header.php
+++ partials/header.php
@@ -25,69 +25,16 @@ $stmt = $mysqli->prepare($ret);
 $stmt->execute(); //ok
 $res = $stmt->get_result();
 while ($logged_in_user = $res->fetch_object()) {
-    /* Count Notifications */
-    $query = "SELECT COUNT(*)  FROM `iResturant_Notification`  WHERE user_id = '$id' AND status = 'Unread'";
-    $stmt = $mysqli->prepare($query);
-    $stmt->execute();
-    $stmt->bind_result($notification_count);
-    $stmt->fetch();
-    $stmt->close();
-
 ?>
     <div class="topbar">
         <!-- Navbar -->
         <nav class="navbar-custom">
             <ul class="list-unstyled topbar-nav float-end mb-0">
 
-                <li class="dropdown notification-list">
-                    <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                        <i data-feather="bell" class="align-self-center topbar-icon"></i>
-                        <span class="badge bg-danger rounded-pill noti-icon-badge"><?php echo $notification_count; ?></span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-end dropdown-lg pt-0">
-
-                        <h6 class="dropdown-item-text font-15 m-0 py-3 border-bottom d-flex justify-content-between align-items-center">
-                            Notifications <span class="badge bg-primary rounded-pill"><?php echo $notification_count; ?></span>
-                        </h6>
-                        <div class="notification-menu" data-simplebar>
-                            <?php
-                            $ret = "SELECT * FROM `iResturant_Notification` WHERE user_id = '$id'";
-                            $stmt = $mysqli->prepare($ret);
-                            $stmt->execute(); //ok
-                            $res = $stmt->get_result();
-                            while ($notification = $res->fetch_object()) {
-                            ?>
-                                <!-- item-->
-                                <a href="notifications" class="dropdown-item py-3">
-                                    <small class="float-end text-muted ps-2"><?php echo date('d-M-Y g:ia', strtotime($notification->created_at)); ?></small>
-                                    <div class="media">
-                                        <div class="avatar-md bg-soft-primary">
-                                            <i data-feather="<?php echo $notification->icon; ?>" class="align-self-center icon-xs"></i>
-                                        </div>
-                                        <div class="media-body align-self-center ms-2 text-truncate">
-                                            <h6 class="my-0 fw-normal text-dark"><?php echo $notification->title; ?></h6>
-                                            <small class="text-muted mb-0"><?php echo substr($notification->details, 0, 15); ?>...</small>
-                                        </div>
-                                        <!--end media-body-->
-                                    </div>
-                                    <!--end media-->
-                                </a>
-                            <?php
-                            } ?>
-                            <!--end-item-->
-
-                            <!--end-item-->
-                        </div>
-                        <!-- All-->
-                        <a href="notifications" class="dropdown-item text-center text-primary">
-                            View all <i class="fi-arrow-right"></i>
-                        </a>
-                    </div>
-                </li>
 
                 <li class="dropdown">
                     <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                        <span class="ms-1 nav-user-name hidden-sm"><?php echo substr($logged_in_user->email, 0, 5); ?>...</span>
+                        <span class="ms-1 nav-user-name hidden-sm">Hello <?php echo substr($logged_in_user->email, 0, 5); ?>...</span>
                         <img src="../public/uploads/user_images/no-profile.png" alt="profile-user" class="rounded-circle thumb-xs" />
                     </a>
                     <div class="dropdown-menu dropdown-menu-end">
