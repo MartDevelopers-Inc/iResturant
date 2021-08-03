cf17d82f460fd0fe70dae67f0842a4711537df9f
MartMbithi <martdevelopers254@gmail.com>
2021-08-03 14:39:44 +0300

    Implement Staff Notification

---
+++ views/staff_notifications.php
@@ -0,0 +1,135 @@
+<?php
+/*
+ * Created on Fri Jul 23 2021
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+session_start();
+require_once('../config/config.php');
+require_once('../config/checklogin.php');
+require_once('../config/codeGen.php');
+staff();
+
+require_once('../partials/head.php');
+?>
+
+<body>
+    <!-- Left Sidenav -->
+    <?php require_once('../partials/staff_sidebar.php'); ?>
+    <!-- end left-sidenav-->
+
+
+    <div class="page-wrapper">
+        <!-- Top Bar Start -->
+        <?php require_once('../partials/staff_header.php'); ?>
+        <!-- Top Bar End -->
+
+        <!-- Page Content-->
+        <div class="page-content">
+            <div class="container-fluid">
+                <!-- Page-Title -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="page-title-box">
+                            <div class="row">
+                                <div class="col">
+                                    <h4 class="page-title">My Notifications</h4>
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="staff_dashboard">Home</a></li>
+                                        <li class="breadcrumb-item"><a href="staff_dashboard">Dashboard</a></li>
+                                        <li class="breadcrumb-item active">Notifications</li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end page title end breadcrumb -->
+
+
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12">
+                        <div class="card">
+                            <!--end card-header-->
+                            <div class="card-body table-responsive">
+                                <div class="">
+                                    <table class="table dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                        <thead>
+                                            <tr>
+                                                <th class="border-top-0">Title</th>
+                                                <th class="border-top-0">Details</th>
+                                                <th class="border-top-0">Manage</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $number = $_SESSION['number'];
+
+                                            $ret = "SELECT * FROM `iResturant_Staff` WHERE number = '$number'  ";
+                                            $stmt = $mysqli->prepare($ret);
+                                            $stmt->execute(); //ok
+                                            $res = $stmt->get_result();
+                                            while ($logged_in_user = $res->fetch_object()) {
+                                                $ret = "SELECT * FROM `iResturant_Notification` WHERE user_id = '$logged_in_user->id' ";
+                                                $stmt = $mysqli->prepare($ret);
+                                                $stmt->execute(); //ok
+                                                $res = $stmt->get_result();
+                                                while ($notifiction = $res->fetch_object()) {
+                                            ?>
+                                                    <tr>
+                                                        <td>
+                                                            <a class="text-primary" href="">
+                                                                <?php echo $notifiction->title; ?>
+                                                            </a>
+                                                        </td>
+                                                        <td>
+                                                            <?php echo $notifiction->details; ?><br>
+                                                            <span class="text-success">Generated: <?php echo date('d M Y g:ia', strtotime($notifiction->created_at)); ?></span>
+                                                        </td>
+                                                        <td>
+                                                            <a href="staff_notifications?clear=<?php echo $notifiction->id; ?>" class="btn btn-sm btn-outline-danger">
+                                                                <i data-feather="trash" class="align-self-center icon-xs ms-1"></i> Delete
+                                                            </a>
+
+                                                        </td>
+                                                    </tr>
+                                            <?php
+                                                }
+                                            }   ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <?php require_once('../partials/footer.php'); ?>
+            <!--end footer-->
+        </div>
+        <!-- end page content -->
+    </div>
+    <!-- end page-wrapper -->
+    <!-- jQuery  -->
+    <?php require_once('../partials/scripts.php'); ?>
+
+</body>
+
+</html>
